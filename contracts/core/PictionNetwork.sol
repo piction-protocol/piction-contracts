a7da389d05b1d92f04538be3a494df8329ce4113
jhSeo <jh.seo@battleent.com>
2019-04-09 18:15:52 +0900

    PictionNetworkCore: ContentsRevenue에서 분배에 사용할 Rate 부분 구현

--- contracts/core/PictionNetwork.sol
+++ contracts/core/PictionNetwork.sol
@@ -7,13 +7,19 @@ import "../utils/ValidValue.sol";
 
 contract PictionNetwork is IPictionNetwork, Ownable, ValidValue {
 
-  // AccountsManager
-  // ContentsManager
+  // Managers: AccountsManager, ContentsManager, PostManager
+  // Core: ContentsRevenue
+  // Tokens: PXL
+  // Storages: AccountsStorage, ContentsStorage, PostStorage
+  // Connectors: ELEConnector, PICConnector
   mapping (string => address) private addressList;
+
+  // ContentsDistributor, UserAdoptionPool, DepositPool, EcosystemFund, SupporterPool, ContensProvider, Translator, Marketer
+  mapping (string => uint256) distributeRate;
 
   /**
     * @dev Address 설정
-    * @param contractName 설정하고자 하는 Address 이름
+    * @param contractName 설정하고자 하는 Contract 이름
     * @param pictionAddress 설정하고자 하는 Address
     */
   function setAddress(
@@ -31,7 +37,7 @@ contract PictionNetwork is IPictionNetwork, Ownable, ValidValue {
 
   /**
     * @dev Address 조회
-    * @param contractName 조회하고자 하는 Address 이름
+    * @param contractName 조회하고자 하는 Contract 이름
     */
   function getAddress(string contractName) 
       external 
@@ -39,5 +45,35 @@ contract PictionNetwork is IPictionNetwork, Ownable, ValidValue {
       returns(address pictionAddress) {
 
       pictionAddress = addressList[contractName];
+  }
+
+  /**
+    * @dev Rate 설정
+    * @param contractName 설정하고자 하는 Contract 이름
+    * @param rate 설정하고자 하는 Rate
+    */
+  function setRate(
+      string contractName, 
+      uint256 rate
+  )
+     external 
+     onlyOwner 
+     validRate(rate) 
+  {
+      distributeRate[contractName] = rate;
+
+      emit SetRate(msg.sender, contractName, rate, TimeLib.currentTime());
+  }
+
+  /**
+    * @dev Rate 조회
+    * @param contractName 조회하고자 하는 Contract 이름
+    */
+  function getRate(string contractName) 
+      external 
+      view 
+      returns(uint256 rate) {
+
+      rate = distributeRate[contractName];
   }
 }
--- contracts/interfaces/IPictionNetwork.sol
+++ contracts/interfaces/IPictionNetwork.sol
@@ -5,10 +5,21 @@ interface IPictionNetwork {
 
     function getAddress(string contractName) external view returns (address pictionAddress);
 
+    function setRate(string contractName, uint256 rate) external;
+
+    function getRate(string contractName) external view returns (uint256 rate);
+
     event SetAddress(
         address indexed from,
         string contractName,
         address pictionAddress,
+        uint256 timestamp
+    );
+
+    event SetRate(
+        address indexed from,
+        string contractName,
+        uint256 rate,
         uint256 timestamp
     );
 }
