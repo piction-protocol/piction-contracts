2b5fd435c329c4543a350aa1eeedac61098f30b9
jhSeo <jh.seo@battleent.com>
2019-04-09 15:44:51 +0900

    PictionNetworkCore: ValidValue Require문 수정

--- contracts/utils/ValidValue.sol
+++ contracts/utils/ValidValue.sol
@@ -7,20 +7,20 @@ contract ValidValue {
   }
 
   modifier validAddress(address _account) {
-      require(_account != address(0));
-      require(_account != address(this));
+      require(_account != address(0), "Invaild address: The address 0 is not allowed.");
+      require(_account != address(this), "Invaild address: This address is Same address as contract.");
       _;
   }
 
   modifier validString(string _str) {
-      require(bytes(_str).length > 0);
+      require(bytes(_str).length > 0, "Invalid String: The string value must be at least one character.");
       _;
   }
 
   modifier validRate(uint256 _rate) {
       uint256 validDecimals = 10 ** 16;
-      require((_rate/validDecimals) > 0);
-      require((_rate/validDecimals) <= 100);
+      require((_rate/validDecimals) > 0, "Out of Range: The ratio must be greater then 0.");
+      require((_rate/validDecimals) <= 100, "Out of Range: The ratio must be less then 100.");
       _;
   }
 }
