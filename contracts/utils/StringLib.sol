f1b4b49cc05c68583ccd2b4f726ce02f9cb8b46f
charls.kim <charls.kim.dev@gmail.com>
2019-04-11 15:34:36 +0900

    String Lib 추가 - 빈 문자열 확인, 문자열 비교 기능

---
+++ contracts/utils/StringLib.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.24;
+
+contract StringLib {
+    /**
+    * @dev 빈 문자열 확인 함수
+    * @param value 확인하고자 하는 string value
+    * @return isEmpty 빈 문자열 확인 결과
+    */
+    function isEmptyString(string value) external pure returns(bool isEmpty) {
+        return (bytes(value).length == 0);
+    }
+
+    /**
+    * @dev 문자열 비교 함수
+    * @param value1 비교하고자 하는 문자열
+    * @param value2 비교하고자 하는 문자열
+    * @return isSameValue 동일 문자열 확인 결과
+    */
+    function compareString(string value1, string value2) external pure returns(bool isSameValue) {
+        return (keccak256(value1) == keccak256(value2));
+    }
+}
