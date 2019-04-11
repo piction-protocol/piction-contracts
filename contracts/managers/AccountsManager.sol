3f1e1324b2899e78ba6bd8ff3d43dc5b727224d9
charls.kim <charls.kim.dev@gmail.com>
2019-04-11 15:42:46 +0900

    계정 생성, 수정, 삭제 및 validation 함수 추가, StringLib 적용, IAccountsStorage 적용

--- contracts/managers/AccountsManager.sol
+++ contracts/managers/AccountsManager.sol
@@ -7,83 +7,127 @@ import "../interfaces/IAccountsStorage.sol";
 import "../interfaces/IAccountsManager.sol";
 import "../utils/ValidValue.sol";
 import "../utils/TimeLib.sol";
+import "../utils/StringLib.sol";
 
 contract AccountsManager is IAccountsManager, Ownable, ValidValue {
     
     string public constant STORAGE_NAME = "AccountsStorage";
+    string public constant CREATE_TAG = "CreateAccount";
+    string public constant UPDATE_TAG = "UpdateAccount";
+    string public constant DELETE_TAG = "DeleteAccount";
 
     IPictionNetwork private pictionNetwork;
     IAccountsStorage private accountsStorage;
 
-    mapping (string => string) private accounts;
-
-    constructor(address piction) validAddress(piction) {
+    constructor(address piction) public validAddress(piction) {
         pictionNetwork = IPictionNetwork(piction);
+
+        require(pictionNetwork.getAddress(STORAGE_NAME) != address(0), "AccountManager deploy failed: Check Piction Network account storage address");
         accountsStorage = IAccountsStorage(pictionNetwork.getAddress(STORAGE_NAME));
     }
 
     /**
     * @dev 계정 생성
     * @param id 사용자 계정 id
-    * @param hash 계정정보로 생성한 hash
-    * @param json 사용자 계정 정보
+    * @param hash 사용자 고유 hash
+    * @param rawData 사용자 계정 정보
+    * @param sender 사용자 주소
     */
-    function createAccount(string id, string hash, string json) external onlyOwner {
+    function createAccount(string id, string hash, string rawData, address sender) 
+        external 
+        onlyOwner validString(id) validString(hash) validString(rawData) validAddress(sender) 
+    {
         require(availableId(id), "Account creation failed: Invalid user id");
-        require(!_isEmptyString(accountsStorage.getStringField(hash)), "Account creation failed: Invalid hash string");
-
-        accounts[id] = hash;
-        accountsStorage.setStringField(hash, json);
+        require(availableAddress(sender), "Account creation failed: Invalid public address");
+        require(StringLib.isEmptyString(accountsStorage.getStringValue(hash)), "Account creation failed: Invalid hash string");
         
-        emit CreateAccount(msg.sender, id, TimeLib.currentTime());
+        accountsStorage.setBooleanValue(id, true, CREATE_TAG);
+        accountsStorage.setAddressRegistration(sender, hash);
+        accountsStorage.setStringValue(hash, rawData, CREATE_TAG);
+    }
+
+    /**
+    * @dev 계정 정보 변경
+    * @param id 사용자 계정 id
+    * @param hash 사용자 고유 hash
+    * @param rawData 사용자 계정 정보
+    * @param sender 사용자 주소
+    */
+    function updateAccount(string id, string hash, string rawData, address sender) 
+        external 
+        onlyOwner validString(id) validString(hash) validString(rawData) validAddress(sender) 
+    {
+        require(accountValidation(id, hash, rawData), "Account update failed: Invalid account info");
+        
+        accountsStorage.setStringValue(hash, rawData, UPDATE_TAG);
+    }
+
+    /**
+    * @dev 계정 삭제
+    * @param id 사용자 계정 id
+    * @param hash 사용자 고유 hash
+    * @param rawData 사용자 계정 정보
+    * @param sender 사용자 주소
+    */
+    function deleteAccount(string id, string hash, string rawData, address sender) 
+        external 
+        onlyOwner validString(id) validString(hash) validString(rawData) validAddress(sender) 
+    {
+        require(accountValidation(id, hash, rawData), "Account delete failed: Invalid account info");
+
+        accountsStorage.setStringValue(hash, "", DELETE_TAG);
+        accountsStorage.setAddressRegistration(sender, "");
+        accountsStorage.setBooleanValue(id, false, DELETE_TAG);
     }
 
     /**
     * @dev 계정 정보 검증
     * @param id 사용자 계정 id
     * @param hash 계정정보로 생성한 hash
-    * @param json 사용자 계정 정보
+    * @param rawData 사용자 계정 정보
+    * @param sender 사용자 주소
     * @return isValid 계정 검증 결과
     */
-    function accountValidation(string id, string hash, string json) 
+    function accountValidation(string id, string hash, string rawData, address sender) 
         public 
-        onlyOwner validString(id) validString(hash) validString(json) 
+        onlyOwner validString(id) validString(hash) validString(rawData) validAddress(sender)
         view 
         returns(bool isValid) 
     {
-        if(!_isEmptyString(accounts[id]) 
-            && _compareString(accounts[id], hash) 
-            && _compareString(json, accountsStorage.getStringField(hash))) 
+        if(!accountsStorage.getBooleanValue(id)
+            && StringLib.compareString(rawData, accountsStorage.getStringValue(hash))
+            && StringLib.compareString(hash, accountsStorage.getAddressRegistration(sender)))
         {
             isValid = true;
         }
     }
 
     /**
-    * @dev 사용 가능 id 정보 조회
+    * @dev 사용자 id 사용가능 여부 조회
     * @param id 사용자 계정 id
-    * @return isAvailable id 사용 가능 여부
+    * @return isAvailable 사용 가능 여부
     */
     function availableId(string id) public onlyOwner validString(id) view returns(bool isAvailable) {
-        return _isEmptyString(accounts[id]);
+        return !accountsStorage.getBooleanValue(id);
     }
 
     /**
-    * @dev 빈 문자열 확인 함수
-    * @param value 확인하고자 하는 string value
-    * @return isEmpty 빈 문자열 확인 결과
+    * @dev 사용자 주소 사용가능 여부 조회
+    * @param sender public address
+    * @return isAvailable 사용 가능 여부
     */
-    function _isEmptyString(string value) private returns(bool isEmpty) {
-        return (bytes(value).length == 0);
+    function availableAddress(address sender) public onlyOwner validAddress(sender) view returns(bool isAvailable) {
+        string memory hash = accountsStorage.getAddressRegistration(sender);
+        return StringLib.isEmptyString(hash);
     }
 
     /**
-    * @dev 문자열 비교 함수
-    * @param value1 비교하고자 하는 문자열
-    * @param value2 비교하고자 하는 문자열
-    * @return isSameValue 동일 문자열 확인 결과
+    * @dev 사용자 주소 사용가능 여부 조회
+    * @param hash 사용자 고유 hash
+    * @return isAvailable 사용 가능 여부
     */
-    function _compareString(string value1, string value2) private returns(bool isSameValue) {
-        return (keccak256(value1) == keccak256(value2));
+    function availableHash(string hash) public onlyOwner validString(hash) view returns(bool isAvailable) {
+        string memory rawData = accountsStorage.getStringValue(hash);
+        return StringLib.isEmptyString(rawData);
     }
 }
