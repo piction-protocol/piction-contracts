9951390787b5b57f18a879a5cbcb8269a56f8103
charls.kim <charls.kim.dev@gmail.com>
2019-04-12 12:51:17 +0900

    Account Storage 삭제

--- contracts/interfaces/IAccountsStorage.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.4.24;
-
-contract IAccountsStorage {
-    function setAddressRegistration(address sender, string hash) public;
-    function getAddressRegistration(address sender) public view returns(string hash);
-}
