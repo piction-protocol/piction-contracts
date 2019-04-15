0141e92ad90183bd49772bcc7e87bdb21807481c
charls.kim <charls.kim.dev@gmail.com>
2019-04-15 11:40:14 +0900

    Account manager 인터페이스 구조 변경

--- contracts/interfaces/IAccountsManager.sol
+++ contracts/interfaces/IAccountsManager.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.4.24;
 
 contract IAccountsManager {
-    function createAccount(string id, string hash, string json) external;
-    function accountValidation(string id, string hash, string json) public view returns (bool isValid);
+    function createAccount(string id, string userHash, string rawData) external;
+    function updateAccount(string id, string userHash, string rawData, address sender) external;
+    function deleteAccount(string id, string userHash, string rawData, address sender) external;
     function availableId(string id) public view returns(bool isAvailable);
-    event CreateAccount(address indexed sender, string id, uint256 timestamp);
+    function availableUserHash(string userHash) public view returns(bool isAvailable);
+    function getUserAddress(string userHash) public view returns(address publicKey);
 }
