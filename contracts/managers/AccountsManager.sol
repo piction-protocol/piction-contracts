386aa69e62a5ee38c228e14aff24fe206788ecbc
charls.kim <charls.kim.dev@gmail.com>
2019-04-15 11:42:11 +0900

    Validation 최소화, user address 조회 함수 추가

--- contracts/managers/AccountsManager.sol
+++ contracts/managers/AccountsManager.sol
@@ -4,7 +4,6 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 import "../interfaces/IStorage.sol";
 import "../interfaces/IPictionNetwork.sol";
-import "../interfaces/IAccountsStorage.sol";
 import "../interfaces/IAccountsManager.sol";
 
 import "../utils/ValidValue.sol";
@@ -20,90 +19,70 @@ contract AccountsManager is IAccountsManager, Ownable, ValidValue {
 
     IStorage private iStorage;
     IPictionNetwork private pictionNetwork;
-    IAccountsStorage private accountsStorage;
 
     constructor(address piction) public validAddress(piction) {
         pictionNetwork = IPictionNetwork(piction);
 
-        require(pictionNetwork.getAddress(STORAGE_NAME) != address(0), "AccountManager deploy failed: Check Piction Network account storage address");
+        require(pictionNetwork.getAddress(STORAGE_NAME) != address(0), "AccountManager deploy failed: Check account storage address");
         iStorage = IStorage(pictionNetwork.getAddress(STORAGE_NAME));
-        accountsStorage = IAccountsStorage(pictionNetwork.getAddress(STORAGE_NAME));
     }
 
     /**
     * @dev 계정 생성
     * @param id 사용자 계정 id
-    * @param hash 사용자 고유 hash
+    * @param userHash 사용자 고유 hash
     * @param rawData 사용자 계정 정보
     * @param sender 사용자 주소
     */
-    function createAccount(string id, string hash, string rawData, address sender) 
+    function createAccount(string id, string userHash, string rawData, address sender) 
         external 
-        onlyOwner validString(id) validString(hash) validString(rawData) validAddress(sender) 
+        onlyOwner validString(id) validString(userHash) validString(rawData) validAddress(sender) 
     {
-        require(availableId(id), "Account creation failed: Invalid user id");
-        require(availableAddress(sender), "Account creation failed: Invalid public address");
-        require(StringLib.isEmptyString(iStorage.getStringValue(hash)), "Account creation failed: Invalid hash string");
+        require(availableId(id), "Account creation failed: Already exists user id");
+        require(iStorage.getAddressValue(userHash) == address(0), "Account creation failed: Already exists user hash and address");
+        require(StringLib.isEmptyString(iStorage.getStringValue(userHash)), "Account creation failed: Already exists userHash and raw data");
         
         iStorage.setBooleanValue(id, true, CREATE_TAG);
-        accountsStorage.setAddressRegistration(sender, hash);
-        iStorage.setStringValue(hash, rawData, CREATE_TAG);
+        iStorage.setAddressValue(userHash, sender, CREATE_TAG);
+        iStorage.setStringValue(userHash, rawData, CREATE_TAG);
     }
 
     /**
     * @dev 계정 정보 변경
     * @param id 사용자 계정 id
-    * @param hash 사용자 고유 hash
+    * @param userHash 사용자 고유 hash
     * @param rawData 사용자 계정 정보
     * @param sender 사용자 주소
     */
-    function updateAccount(string id, string hash, string rawData, address sender) 
+    function updateAccount(string id, string userHash, string rawData, address sender) 
         external 
-        onlyOwner validString(id) validString(hash) validString(rawData) validAddress(sender) 
+        onlyOwner validString(id) validString(userHash) validString(rawData) validAddress(sender) 
     {
-        require(accountValidation(id, hash, rawData), "Account update failed: Invalid account info");
+        require(!availableId(id), "Update account failed: Invalid user id");
+        require(iStorage.getAddressValue(userHash) == sender, "Update account failed: Invalid user hash and address");
+        require(StringLib.isEmptyString(iStorage.getStringValue(userHash)), "Update account failed: Invalid user hash and raw data");
         
-        iStorage.setStringValue(hash, rawData, UPDATE_TAG);
+        iStorage.setStringValue(userHash, rawData, UPDATE_TAG);
     }
 
     /**
     * @dev 계정 삭제
     * @param id 사용자 계정 id
-    * @param hash 사용자 고유 hash
+    * @param userHash 사용자 고유 hash
     * @param rawData 사용자 계정 정보
     * @param sender 사용자 주소
     */
-    function deleteAccount(string id, string hash, string rawData, address sender) 
+    function deleteAccount(string id, string userHash, string rawData, address sender) 
         external 
-        onlyOwner validString(id) validString(hash) validString(rawData) validAddress(sender) 
+        onlyOwner validString(id) validString(userHash) validString(rawData) validAddress(sender) 
     {
-        require(accountValidation(id, hash, rawData), "Account delete failed: Invalid account info");
+        require(!availableId(id), "Delete account failed: Invalid user id");
+        require(iStorage.getAddressValue(userHash) == sender, "Delete account failed: Invalid user hash and address");
+        require(StringLib.isEmptyString(iStorage.getStringValue(userHash)), "Delete account failed: Invalid user hash and raw data");
 
-        iStorage.setStringValue(hash, "", DELETE_TAG);
-        accountsStorage.setAddressRegistration(sender, "");
-        iStorage.setBooleanValue(id, false, DELETE_TAG);
-    }
-
-    /**
-    * @dev 계정 정보 검증
-    * @param id 사용자 계정 id
-    * @param hash 계정정보로 생성한 hash
-    * @param rawData 사용자 계정 정보
-    * @param sender 사용자 주소
-    * @return isValid 계정 검증 결과
-    */
-    function accountValidation(string id, string hash, string rawData, address sender) 
-        public 
-        onlyOwner validString(id) validString(hash) validString(rawData) validAddress(sender)
-        view 
-        returns(bool isValid) 
-    {
-        if(!iStorage.getBooleanValue(id)
-            && StringLib.compareString(rawData, iStorage.getStringValue(hash))
-            && StringLib.compareString(hash, accountsStorage.getAddressRegistration(sender)))
-        {
-            isValid = true;
-        }
+        iStorage.deleteBooleanValue(id, DELETE_TAG);
+        iStorage.deleteAddressValue(userHash, DELETE_TAG);
+        iStorage.deleteStringValue(userHash, DELETE_TAG);
     }
 
     /**
@@ -111,27 +90,25 @@ contract AccountsManager is IAccountsManager, Ownable, ValidValue {
     * @param id 사용자 계정 id
     * @return isAvailable 사용 가능 여부
     */
-    function availableId(string id) public onlyOwner validString(id) view returns(bool isAvailable) {
+    function availableId(string id) public validString(id) view returns(bool isAvailable) {
         return !iStorage.getBooleanValue(id);
     }
 
     /**
     * @dev 사용자 주소 사용가능 여부 조회
-    * @param sender public address
+    * @param userHash 사용자 고유 hash
     * @return isAvailable 사용 가능 여부
     */
-    function availableAddress(address sender) public onlyOwner validAddress(sender) view returns(bool isAvailable) {
-        string memory hash = accountsStorage.getAddressRegistration(sender);
-        return StringLib.isEmptyString(hash);
+    function availableUserHash(string userHash) public validString(userHash) view returns(bool isAvailable) {
+        return (StringLib.isEmptyString(iStorage.getStringValue(userHash)) && iStorage.getAddressValue(userHash) == address(0));
     }
 
     /**
-    * @dev 사용자 주소 사용가능 여부 조회
-    * @param hash 사용자 고유 hash
-    * @return isAvailable 사용 가능 여부
+    * @dev 사용자 주소 조회
+    * @param userHash 사용자 고유 hash
+    * @return publicKey 사용자 주소
     */
-    function availableHash(string hash) public onlyOwner validString(hash) view returns(bool isAvailable) {
-        string memory rawData = iStorage.getStringValue(hash);
-        return StringLib.isEmptyString(rawData);
+    function getUserAddress(string userHash) public validString(userHash) view returns(address publicKey) {
+        return iStorage.getAddressValue(userHash);
     }
 }
