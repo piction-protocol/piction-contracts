eb2c8ab204348d069febc87eacb8ea1a209aabf2
charls.kim <charls.kim.dev@gmail.com>
2019-04-15 15:28:37 +0900

    event 추가, 파라미터 추가

--- contracts/interfaces/IAccountsManager.sol
+++ contracts/interfaces/IAccountsManager.sol
@@ -1,10 +1,11 @@
 pragma solidity ^0.4.24;
 
 contract IAccountsManager {
-    function createAccount(string id, string userHash, string rawData) external;
+    function createAccount(string id, string userHash, string rawData, address sender) external;
     function updateAccount(string id, string userHash, string rawData, address sender) external;
     function deleteAccount(string id, string userHash, string rawData, address sender) external;
     function availableId(string id) public view returns(bool isAvailable);
     function availableUserHash(string userHash) public view returns(bool isAvailable);
     function getUserAddress(string userHash) public view returns(address publicKey);
+    event setAccount(address indexed pictionNetwork, address indexed user, string id, string tag);
 }
