fb3746c0a9cfac8fd56dc0e6468f0479b9038b34
변동명 <dm.byun@thewhalegames.co.kr>
2019-04-16 19:01:08 +0900

    Contents 테스트 코드 진행 중

--- contracts/interfaces/IPostManager.sol
+++ contracts/interfaces/IPostManager.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.4.24;
 interface IPostManager {
     function createPost(string userHash, string contentsHash, string postHash, string rawData) external;
     function updatePost(string userHash, string contentsHash, string postHash, string rawData) external;
-    function deletePost(string userHash, string contentsHash) external;
+    function deletePost(string userHash, string contentsHash, string postHash) external;
     function movePost(string userHash, string beforContentsHash, string afterContentsHash, string postHash) external;
     
-    function getPostWriter(string contentsHash) external view returns(address writer);
-    function getPostRawData(string contentsHash) external view returns(string rawData);
+    function getPostWriter(string postHash) external view returns(address writer);
+    function getPostRawData(string postHash) external view returns(string rawData);
     function getContentsHash(string postHash) external view returns(string contentsHash);
 }
--- test/ContentsPostManager.test.js
+++ test/ContentsPostManager.test.js
@@ -30,7 +30,7 @@ contract("manager", function (accounts) {
     let contentsStorage;
     let relationStorage;
 
-    let tagAccountMnanager = "AccountsManager";
+    let tagAccountsManager = "AccountsManager";
     let tagContentsManager = "ContentsManager";
     let tagPostManager = "PostManager";
     let tagAccountsStorage = "AccountsStorage";
@@ -41,28 +41,49 @@ contract("manager", function (accounts) {
         it("deploy contracts", async () => {
             pictionNetwork = await PictionNetwork.new({from: owner}).should.be.fulfilled;
 
-            accountsManager = await AccountsManager.new(pictionNetwork.address, {from: owner}).should.be.fulfilled;
-            contentsManager = await ContentsManager.new(pictionNetwork.address, {from: owner}).should.be.fulfilled;
-            postManager = await PostManager.new(pictionNetwork.address, {from: owner}).should.be.fulfilled;
-
             accountsStorage = await AccountsStorage.new(pictionNetwork.address, {from: owner}).should.be.fulfilled;
-            contentsStorage = await ContentsStorage.new(pictionNetwork.address, {from: owner}).should.be.fulfilled;
-            relationStorage = await RelationStorage.new(pictionNetwork.address, {from: owner}).should.be.fulfilled;
-        });
-
-        it("set PictionNetwork Contracts", async () => {
-            await pictionNetwork.setAddress(tagAccountMnanager, accountsManager.address, {from: owner}).should.be.fulfilled;
+            await pictionNetwork.setAddress(tagAccountsStorage, accountsStorage.address, {from: owner}).should.be.fulfilled;
+            accountsManager = await AccountsManager.new(pictionNetwork.address, {from: owner}).should.be.fulfilled;
+            await pictionNetwork.setAddress(tagAccountsManager, accountsManager.address, {from: owner}).should.be.fulfilled;
+            contentsManager = await ContentsManager.new(pictionNetwork.address, {from: owner}).should.be.fulfilled;
             await pictionNetwork.setAddress(tagContentsManager, contentsManager.address, {from: owner}).should.be.fulfilled;
+            postManager = await PostManager.new(pictionNetwork.address, {from: owner}).should.be.fulfilled;
             await pictionNetwork.setAddress(tagPostManager, postManager.address, {from: owner}).should.be.fulfilled;
 
-            await pictionNetwork.setAddress(tagAccountsStorage, accountsStorage.address, {from: owner}).should.be.fulfilled;
+            contentsStorage = await ContentsStorage.new(pictionNetwork.address, {from: owner}).should.be.fulfilled;
             await pictionNetwork.setAddress(tagContentsStorage, contentsStorage.address, {from: owner}).should.be.fulfilled;
+            relationStorage = await RelationStorage.new(pictionNetwork.address, {from: owner}).should.be.fulfilled;
             await pictionNetwork.setAddress(tagRelationStorage, relationStorage.address, {from: owner}).should.be.fulfilled;
         });
 
-        //todo
-        //1. accountsManager 유저 생성 및 검증
-        //2. contentsManager 콘텐츠 생성 및 검증
-        //3. postManager Post 생성 및 검증
+        it("contentsManager test start", async () => {    
+            //accountsManager 계정 생성
+            await accountsManager.createAccount('0', 'userHash', 'accountRawData', user1, {from: owner}).should.be.fulfilled;
+            
+            //콘텐츠 생성 검증
+            //비정상 유저 해시
+            await contentsManager.createContents("FakeUserHash", "contentsHash", "contentsRawData", {from: user1}).should.be.rejected;
+            //비정상 유저 Address
+            await contentsManager.createContents("userHash", "contentsHash", "contentsRawData", {from: user2}).should.be.rejected;
+
+            //contentsManager 콘텐츠 생성
+            await contentsManager.createContents("userHash", "contentsHash", "contentsRawData", {from: user1}).should.be.fulfilled;
+
+            //중복 생성
+
+            //업데이트
+
+            //삭제
+        });
+
+        it("postManager test start", async () => {
+
+            //Post 삭제된 Contents 기반 생성
+            //Post 생성
+            //Post Update
+            //Post Move 임의 Contents
+            //Post Move
+            //Post 삭제
+        });
     });
 });
