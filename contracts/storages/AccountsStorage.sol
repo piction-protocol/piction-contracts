765cea41c237c86992b1ace1689751295c2def0b
charls.kim <charls.kim.dev@gmail.com>
2019-04-17 12:55:01 +0900

    modifier 제거(가스비용 영향), require문으로 대체

--- contracts/storages/AccountsStorage.sol
+++ contracts/storages/AccountsStorage.sol
@@ -12,84 +12,111 @@ contract AccountsStorage is Storage, Ownable, ValidValue {
 
     IPictionNetwork private pictionNetwork;
 
-    modifier onlyAccountsManager(address sender) {
-        require(sender != address(0), "Invaild address: Address 0 is not allowed.");
-        require(sender != address(this), "Invaild address: Same address as AccountsStorage contact");
-        require(pictionNetwork.getAddress(MANAGER_NAME) == sender, "Invalid address: Access denied.");
-        _;
-    }
-
-    modifier readOnlyRole(address sender) {
-        require(sender != address(0), "Invaild address: Address 0 is not allowed.");
-        require(sender != address(this), "Invaild address: Same address as AccountsStorage contact");
-        require(pictionNetwork.getAddress(MANAGER_NAME) == sender, "Invalid address: Access denied.");
-        
-        // Piction network에 read only 계정의 주소가 확정되면 설정
-        // require(pictionNetwork.getAddress("") == addr, "Invalid address: Access denied.");
-        _;
-    }
-
     constructor(address piction) public validAddress(piction) {
         pictionNetwork = IPictionNetwork(piction);
     }
     
-    function setBooleanValue(string key, bool value, string tag) public onlyAccountsManager(msg.sender) {
+    /**
+     *  TODO setter, getter의 require는 storage 통합할 경우 수정 필요
+     */
+
+    function setBooleanValue(string key, bool value, string tag) public {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+        
         super.setBooleanValue(key, value, tag);
     }
 
-    function setBytesValue(string key, bytes value, string tag) public onlyAccountsManager(msg.sender)  {
+    function setBytesValue(string key, bytes value, string tag) public {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+        
         super.setBytesValue(key, value, tag);
     }
 
-    function setStringValue(string key, string value, string tag) public onlyAccountsManager(msg.sender) {
+    function setStringValue(string key, string value, string tag) public  {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+        
         super.setStringValue(key, value, tag);
     }
 
-    function setUintValue(string key, uint256 value, string tag) public onlyAccountsManager(msg.sender) {
+    function setUintValue(string key, uint256 value, string tag) public  {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+        
         super.setUintValue(key, value, tag);
     }
 
-    function setAddressValue(string key, address value, string tag) public onlyAccountsManager(msg.sender)  {
+    function setAddressValue(string key, address value, string tag) public {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+        
         super.setAddressValue(key, value, tag);
     }
 
-    function getBooleanValue(string key) public readOnlyRole(msg.sender) view returns(bool value) {
+    function getBooleanValue(string key) public view returns(bool value) {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+        // Piction network에 read only 계정의 주소가 확정되면 설정
+        // require(pictionNetwork.getAddress("") == addr, "Invalid address: Access denied.");
+
         return super.getBooleanValue(key);
     }
 
-    function getBytesValue(string key) public readOnlyRole(msg.sender) view returns(bytes value) {
+    function getBytesValue(string key) public view returns(bytes value) {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+        // Piction network에 read only 계정의 주소가 확정되면 설정
+        // require(pictionNetwork.getAddress("") == addr, "Invalid address: Access denied.");
+
         return super.getBytesValue(key);
     }
 
-    function getStringValue(string key) public readOnlyRole(msg.sender) view returns(string value) {
+    function getStringValue(string key) public view returns(string value) {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+        // Piction network에 read only 계정의 주소가 확정되면 설정
+        // require(pictionNetwork.getAddress("") == addr, "Invalid address: Access denied.");
+
         return super.getStringValue(key);
     }
 
-    function getUintValue(string key) public readOnlyRole(msg.sender) view returns(uint256 value) {
+    function getUintValue(string key) public view returns(uint256 value) {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+        // Piction network에 read only 계정의 주소가 확정되면 설정
+        // require(pictionNetwork.getAddress("") == addr, "Invalid address: Access denied.");
+
         return super.getUintValue(key);
     }
 
-    function getAddressValue(string key) public readOnlyRole(msg.sender) view returns(address value) {
+    function getAddressValue(string key) public  view returns(address value) {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+        // Piction network에 read only 계정의 주소가 확정되면 설정
+        // require(pictionNetwork.getAddress("") == addr, "Invalid address: Access denied.");
+
         return super.getAddressValue(key);
     }
 
-    function deleteBooleanValue(string key, string tag) public onlyAccountsManager(msg.sender) {
+    function deleteBooleanValue(string key, string tag) public {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+
         super.deleteBooleanValue(key, tag);
     }
 
-    function deleteBytesValue(string key, string tag) public onlyAccountsManager(msg.sender)  {
+    function deleteBytesValue(string key, string tag) public {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+
         super.deleteBytesValue(key, tag);
     }
 
-    function deleteStringValue(string key, string tag) public onlyAccountsManager(msg.sender) {
+    function deleteStringValue(string key, string tag) public {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+
         super.deleteStringValue(key, tag);
     }
 
-    function deleteUintValue(string key, string tag) public onlyAccountsManager(msg.sender) {
+    function deleteUintValue(string key, string tag) public {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+
         super.deleteUintValue(key, tag);
     }
 
-    function deleteAddressValue(string key, string tag) public onlyAccountsManager(msg.sender)  {
+    function deleteAddressValue(string key, string tag) public {
+        require(pictionNetwork.getAddress(MANAGER_NAME) == msg.sender, "Invalid address: Access denied.");
+        
         super.deleteAddressValue(key, tag);
     }
 }
