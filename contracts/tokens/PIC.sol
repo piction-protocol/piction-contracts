afc0ca2ae91a662b53fdd31e0d7446686e1e468e
charls.kim <charls.kim.dev@gmail.com>
2019-04-24 15:48:59 +0900

    require 메시지 추가, initial value제거, mint 함수 initial value parameter 추가

--- contracts/tokens/PIC.sol
+++ contracts/tokens/PIC.sol
@@ -2,13 +2,11 @@ pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-import "../interface/IPIC.sol";
+import "../interfaces/IPIC.sol";
 import "../utils/ExtendsOwnable.sol";
 
 contract PIC is IPIC, ExtendsOwnable {
     using SafeMath for uint256;
-
-    uint256 public constant initialValue = 10000 * (10 ** 18);
 
     mapping (string => uint256) private _totalSupply;
     mapping (string => mapping (address => uint256)) private _balances;
@@ -26,8 +24,8 @@ contract PIC is IPIC, ExtendsOwnable {
     }
 
     function transfer(string contentsHash, address from, address to, uint256 value) public returns (bool) {
-        require(to != address(0));
-        require(_totalSupply[contentsHash] > 0);
+        require(to != address(0), "PIC transfer 0");
+        require(_totalSupply[contentsHash] > 0, "PIC transfer 1");
         
         _balances[contentsHash][from] = _balances[contentsHash][from].sub(value);
         _balances[contentsHash][to] = _balances[contentsHash][to].add(value);
@@ -37,22 +35,21 @@ contract PIC is IPIC, ExtendsOwnable {
     }
 
     function burn(string contentsHash, address from, uint256 value) external onlyOwner {
-        require(from != address(0));
-        require(_totalSupply[contentsHash] > 0);
+        require(from != address(0), "PIC burn 0");
+        require(_totalSupply[contentsHash] > 0, "PIC burn 1");
 
         _totalSupply[contentsHash] = _totalSupply[contentsHash].sub(value);
         _balances[contentsHash][from] = _balances[contentsHash][from].sub(value);
-        emit Transfer(from, address(0), value);
+        emit Transfer(from, address(0), value, contentsHash);
     }
 
-    function mint(string contentsHash) external onlyOwner {
-        require(_totalSupply[contentsHash] == 0);
+    function mint(string contentsHash, uint256 initialValue) external onlyOwner {
+        require(_totalSupply[contentsHash] == 0, "PIC mint 0");
 
         _totalSupply[contentsHash] = _totalSupply[contentsHash].add(initialValue);
         _balances[contentsHash][msg.sender] = _balances[contentsHash][msg.sender].add(initialValue);
+        emit Transfer(address(0), msg.sender, initialValue, contentsHash);
     }
-
-    event Transfer(address indexed from, address indexed to, uint256 value, string contentsHash);
 
     // TODO approve and call과 같은 기능이 필요한지는 sale contract 만들면서 생각해야함
     // approve and call이 들어갈 경우 transferFrom, _allowed mapping 필요..
