6ff275dbee8c48d66795934a779f757148042433
charls.kim <charls.kim.dev@gmail.com>
2019-04-25 17:35:46 +0900

    개별 조회 함수를 전체 정보를 넘기도록 수정

--- contracts/interfaces/ISupportSaleManager.sol
+++ contracts/interfaces/ISupportSaleManager.sol
@@ -5,8 +5,5 @@ interface ISupportSaleManager {
     function cancelPicSale(string contentsHash) external;
     function refundPic(string contentsHash) external;
     function withDrawPic(string contentsHash) external;
-    function remainingBalance(string contentsHash) external view returns (uint256 picBalance, uint256 pxlBalance);
-    function getPicPrice(string contentsHash) external view returns (uint256 picPrice);
-    function getEndTime(string contentsHash) external view returns (uint256 endTime);
     function getSaleValue(string contentsHash) external view returns (uint256 picPrice, uint256 maxcap, uint256 endtime, uint256 pxlRaised);
 }
