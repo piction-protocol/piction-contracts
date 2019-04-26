e39c958edd06a24bb1e440aadda8ed01b9fe0cf4
변동명 <dm.byun@thewhalegames.co.kr>
2019-04-26 11:42:55 +0900

    VaildValue require string 변경

--- contracts/utils/ValidValue.sol
+++ contracts/utils/ValidValue.sol
@@ -2,24 +2,29 @@ pragma solidity ^0.4.24;
 
 contract ValidValue {
   modifier validRange(uint256 value) {
-      require(value > 0, "Out of Range: The value must be greater than 0.");
+      //Out of Range: The value must be greater than 0.
+      require(value > 0, "ValidValue validRange 0");
       _;
   }
 
   modifier validAddress(address account) {
-      require(account != address(0), "Invaild address: The address 0 is not allowed.");
-      require(account != address(this), "Invaild address: This address is Same address as current contract.");
+      //Invaild address: The address 0 is not allowed.
+      require(account != address(0), "ValidValue validAddress 0");
+      //Invaild address: This address is Same address as current contract.
+      require(account != address(this), "ValidValue validAddress 1");
       _;
   }
 
   modifier validString(string str) {
-      require(bytes(str).length > 0, "Invalid String: The string value must be at least one character.");
+      //Invalid String: The string value must be at least one character.
+      require(bytes(str).length > 0, "ValidValue validString 0");
       _;
   }
 
   modifier validRate(uint256 rate) {
       uint256 validDecimals = 10 ** 18;
-      require(rate <= validDecimals, "Out of Range: The ratio must be less than 1.");
+      //Out of Range: The ratio must be less than 1.
+      require(rate <= validDecimals, "ValidValue validRate 0");
       _;
   }
 }
