df2540936ebcaa151da0366b347f275d390c7360
jhSeo <jh.seo@battleent.com>
2019-04-26 17:36:51 +0900

    PictionNetworkCore: Code style 적용

--- contracts/interfaces/IPictionNetwork.sol
+++ contracts/interfaces/IPictionNetwork.sol
@@ -5,20 +5,7 @@ interface IPictionNetwork {
     function getRate(string contractName) external view returns (uint256 rate);
     function getContentsDistributor(string cdName) external view returns (address cdAddress);
 
-    event SetAddress(
-        address indexed from,
-        string contractName,
-        address pictionAddress
-    );
-
-    event SetRate(
-        address indexed from,
-        string contractName,
-        uint256 rate
-    );
-
-    event SetContentsDistributor(
-        string cdName,
-        address cdAddress
-    );
+    event SetAddress(address indexed from, string contractName, address pictionAddress);
+    event SetRate(address indexed from, string contractName, uint256 rate);
+    event SetContentsDistributor(string cdName, address cdAddress);
 }
