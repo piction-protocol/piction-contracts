a3f5a62979dd0ec74939a9917b24ab033927a642
charls.kim <charls.kim.dev@gmail.com>
2019-05-15 17:00:28 +0900

    user adoption pool interface, contract 추가

---
+++ contracts/pools/UserAdoptionPool.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
+import "../interfaces/IPictionNetwork.sol";
+import "../interfaces/IUserAdoptionPool.sol";
+
+contract UserAdoptionPool is Ownable, IUserAdoptionPool {
+
+    string private constant PXL = "PXL";
+
+    IERC20 public iPxl;
+    IPictionNetwork private iPictionNetwork;
+    
+    constructor(address pictionNetwork) public validAddress(pictionNetwork) {
+        iPictionNetwork = IPictionNetwork(pictionNetwork);
+        iPxl = iPictionNetwork.getAddress(PXL);
+    }
+    
+    function refundPxl() external onlyOwner {
+        uint256 amount = iPxl.balanceOf(address(this));
+
+        iPxl.transfer(msg.sender, amount);
+    }
+}
