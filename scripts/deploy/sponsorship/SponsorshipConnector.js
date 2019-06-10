7df00b21401c53c3381ca6e11b0b46e229b1f2a6
charls.kim <charls.kim.dev@gmail.com>
2019-06-10 11:00:53 +0900

    배포 스크립트 작성

---
+++ scripts/deploy/sponsorship/SponsorshipConnector.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = JSON.parse(fs.readFileSync('build/contracts/SponsorshipConnector.json'));
+const contract = new caver.klay.Contract(input.abi);
+
+module.exports = async () => {
+    log(`>>>>>>>>>> [SponsorshipConnector] <<<<<<<<<<`);
+
+    console.log('> Deploying SponsorshipConnector.');
+
+    const pxl = process.env.PXL_ADDRESS;
+
+    if (!pxl) {
+        error('PXL is not deployed!! Please after PXL deployment.');
+        return;
+    }
+
+    let instance = await contract.deploy({
+        data: input.bytecode,
+        arguments: [pxl]
+    }).send({
+        from: caver.klay.accounts.wallet[0].address,
+        gas: gasLimit,
+        gasPrice: gasPrice
+    }); 
+
+    info(`SponsorshipConnector_ADDRESS: ${instance.contractAddress}`);
+    log(`-------------------------------------------------------------------`);
+} 
