526ad2618abd0e0009658d2fef606b9c6d21c6b0
charls.kim <charls.kim.dev@gmail.com>
2019-06-14 17:15:27 +0900

    구독 관련 인터페이스 추가

--- contracts/interfaces/IProjectManager.sol
+++ contracts/interfaces/IProjectManager.sol
@@ -4,4 +4,6 @@ interface IProjectManager {
     function getWriter(string projectHash) external view returns(address writer);
     function getProjectRawData(string projectHash) external view returns(string rawData);
     function getUserHash(string projectHash) external view returns(string userHash);
+    function subscription(string cdName, address user, uint256 amount, bytes data) external;
+    function isSubscribing(string subscriptionHash) external view returns(bool);
 }
