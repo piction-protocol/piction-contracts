363b43d793de77adb8e6692d9ff893111704ddc3
charls.kim <charls.kim.dev@gmail.com>
2019-06-26 18:06:07 +0900

    사용하지 않는 컨트렉트 삭제

--- contracts/interfaces/IProjectManager.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.24;
-
-interface IProjectManager {
-    function getWriter(string projectHash) external view returns(address writer);
-    function getProjectRawData(string projectHash) external view returns(string rawData);
-    function getUserHash(string projectHash) external view returns(string userHash);
-}
