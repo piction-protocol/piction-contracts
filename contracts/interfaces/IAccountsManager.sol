72a1015e445cdef04011aa37176123bea33a1aa1
charls.kim <charls.kim.dev@gmail.com>
2019-06-26 18:07:29 +0900

    사용하지 않는 컨트렉트 제거

--- contracts/interfaces/IAccountsManager.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.24;
-
-contract IAccountsManager {    
-    function availableEmail(string email) public view returns(bool isAvailable);
-    function availableUserHash(string userHash) public view returns(bool isAvailable);
-    function getUserAddress(string userHash) public view returns(address publicKey);
-    function accountValidation(string userHash, string rawData) external view returns(bool isValid);
-}
--- contracts/interfaces/IPostManager.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.24;
-
-interface IPostManager {
-    function getPostWriter(string postHash) external view returns(address writer);
-    function getPostRawData(string postHash) external view returns(string rawData);
-    function getProjectHash(string postHash) external view returns(string projectHash);
-}
