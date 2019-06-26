07cd3bc5f9c7275984f05a66494779a9ad9d80de
charls.kim <charls.kim.dev@gmail.com>
2019-06-26 18:07:54 +0900

    분배 컨트렉트 인터페이스 수정

--- contracts/interfaces/IContentsRevenue.sol
+++ contracts/interfaces/IContentsRevenue.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.24;
 
 interface IContentsRevenue {
-    function calculateDistributionPxl(uint256 distributionRate, string contentHash, uint256 amount) external view returns (address[] memory addresses, uint256[] memory amounts);
+    function calculateDistributionPxl(uint256 distributionRate, address cp, uint256 amount) external view returns (address[] memory addresses, uint256[] memory amounts);
 }
