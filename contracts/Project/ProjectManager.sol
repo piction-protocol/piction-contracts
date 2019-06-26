a40cd28613f591d388ea9c45164697b4a18fa968
charls.kim <charls.kim.dev@gmail.com>
2019-06-26 18:52:33 +0900

    uri 파라미터 명 수정

--- contracts/Project/ProjectManager.sol
+++ contracts/Project/ProjectManager.sol
@@ -5,37 +5,37 @@ import "./Project.sol";
 
 contract ProjectManager is ValidValue {
 
-    mapping(string => bool) private urlChecker;
+    mapping(string => bool) private uriChecker;
 
     /**
      * @dev 프로젝트 생성 
-     * @param url 프로젝트 고유한 url 정보
+     * @param uri 프로젝트 고유한 uri 정보
      * @param title 프로젝트 이름
      * @param subscriptionPrice 프로젝트 구독 가격
      */
     function createProject(
-        string url, 
+        string uri, 
         string title, 
         uint256 subscriptionPrice
     ) 
         external
-        validString(url) 
+        validString(uri) 
         validString(title)
     {
-        require(!urlChecker[url], "ProjectManager createProject 0");
+        require(!uriChecker[uri], "ProjectManager createProject 0");
 
-        address project = new Project(url, title, subscriptionPrice, msg.sender);
+        address project = new Project(uri, title, subscriptionPrice, msg.sender);
 
         emit DeployProject(msg.sender, project, now * 1000);
     }
 
     /**
-     * @dev 프로젝트 url 중복 확인
-     * @param url 프로젝트 고유한 url 정보
-     * @return url 중복 여부
+     * @dev 프로젝트 uri 중복 확인
+     * @param uri 프로젝트 고유한 uri 정보
+     * @return uri 중복 여부
      */
-    function isExistedUrl(string url) external view returns(bool) {
-        return urlChecker[url];
+    function isExisteduri(string uri) external view returns(bool) {
+        return uriChecker[uri];
     }
 
     event DeployProject(address indexed sender, address indexed projectAddress, uint256 timestamp);
