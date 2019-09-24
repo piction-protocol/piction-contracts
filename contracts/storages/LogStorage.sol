8044239909c1c6af35bbaf8edf38b27964c03351
charls.kim <charls.kim.dev@gmail.com>
2019-09-24 12:17:31 +0900

    Add ExtendsOwable to LogStorage

--- contracts/storages/LogStorage.sol
+++ contracts/storages/LogStorage.sol
@@ -1,7 +1,9 @@
 pragma solidity ^0.4.24;
 
 import "../interfaces/IProxy.sol";
+import "../utils/ExtendsOwnable.sol";
 
-contract LogStorage is IProxy {
+
+contract LogStorage is IProxy, ExtendsOwnable {
 
 }
