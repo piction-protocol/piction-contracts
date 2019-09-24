59fad28b08320682c3adfa3961719f5268029a25
charls.kim <charls.kim.dev@gmail.com>
2019-09-24 12:18:38 +0900

    Add contract event to LogStorage

--- contracts/storages/LogStorage.sol
+++ contracts/storages/LogStorage.sol
@@ -6,4 +6,13 @@ import "../utils/ExtendsOwnable.sol";
 
 contract LogStorage is IProxy, ExtendsOwnable {
 
+
+    event SignIn(address indexed user, string platform);
+    event SignUp(address indexed user, string platform);
+    event View(address indexed project, address indexed user, uint256 postId, string platform);
+    event Subscription(address indexed project, address indexed user, uint256 price, string platform);
+    event UnSubscription(address indexed project, address indexed user, string platform);
+    event Like(address indexed project, address indexed user, uint256 postId, string platform);
+    event Sponsorship(address indexed sponsor, address indexed creator, uint256 amount, string platform);
+    event SearchTag(uint256 indexed tagId, address indexed user, string tag, string platform);
 }
