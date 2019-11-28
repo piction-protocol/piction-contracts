3473db9c20dfc28ab2fee73173ed8e78a70944a2
charls.kim <charls.kim.dev@gmail.com>
2019-11-28 15:53:59 +0900

    사용하지 않는 contract 제거

--- contracts/interfaces/IValidation.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.24;
-
-interface IValidation {
-
-    function stringValidation(string str) external view returns(bool);
-
-    function accountValidation(address user) external view returns (bool);
-}
