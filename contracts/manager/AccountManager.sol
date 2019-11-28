b71af044defb63b2c43d0492f935422855a0ab9f
charls.kim <charls.kim.dev@gmail.com>
2019-11-28 11:57:24 +0900

    주석 추가

--- contracts/manager/AccountManager.sol
+++ contracts/manager/AccountManager.sol
@@ -4,7 +4,10 @@ import "../utils/ValidValue.sol";
 import "../utils/ExtendsOwnable.sol";
 import "../interfaces/IPictionNetwork.sol";
 
-
+/**
+ * @title AccountManager
+ * @dev Piction 계정 정보 관리
+ */
 contract AccountManager is ExtendsOwnable, ValidValue {
 
     struct account {
@@ -22,6 +25,11 @@ contract AccountManager is ExtendsOwnable, ValidValue {
         pictionNetwork = IPictionNetwork(pictionNetworkAddress);
     }
 
+    /**
+      * @dev Piction 회원 가입
+      * @param loginId 사용자 login id
+      * @param email 사용자 email 주소
+      */
     function signup(string loginId, string email) external validString(loginId)  validString(email) {
         require(!accounts[msg.sender].isRegistered, "AccountManager signup 0");
         require(!isDuplicateString[loginId], "AccountManager signup 1");
@@ -37,6 +45,12 @@ contract AccountManager is ExtendsOwnable, ValidValue {
         emit Signup(msg.sender, loginId, email);
     }
 
+    /**
+      * @dev 기존 가입 유저 migration - Piction 계정 실행
+      * @param user 사용자 public address
+      * @param loginId 사용자 login id
+      * @param email 사용자 email 주소
+      */
     function migration(address user, string loginId, string email) external onlyOwner validString(loginId) validString(email) {
         require(!accounts[user].isRegistered, "AccountManager migration 0");
         require(!isDuplicateString[loginId], "AccountManager migration 1");
@@ -52,14 +66,29 @@ contract AccountManager is ExtendsOwnable, ValidValue {
         emit Migration(msg.sender, user, loginId, email);
     }
 
+    /**
+      * @dev Piction에 가입한 유저 확인
+      * @param user 사용자 public address
+      * @return 사용자 가입 유무
+      */
     function accountValidation(address user) external view returns (bool) {
         return accounts[user].isRegistered;
     }
 
+    /**
+      * @dev Piction에 가입한 정보 확인
+      * @param str 사용자 login id 또는 email 주소
+      * @return 사용자 고유 정보 중복 여부
+      */
     function stringValidation(string str) external view returns (bool) {
         return isDuplicateString[str];
     }
 
+    /**
+      * @dev Piction에 가입 된 사용자 정보 확인
+      * @param user 사용자 public address
+      * @return 사용자 계정 정보
+      */
     function getAccount(address user) external view returns (bool, string, string) {
         return (accounts[user].isRegistered, accounts[user].loginId, accounts[user].email);
     }
--- contracts/project/ProjectManager.sol
+++ contracts/project/ProjectManager.sol
@@ -5,7 +5,10 @@ import "../utils/ExtendsOwnable.sol";
 import "../interfaces/IValidation.sol";
 import "../interfaces/IPictionNetwork.sol";
 
-
+/**
+ * @title ProjectManager
+ * @dev 작가가 생성한 Project 관리
+ */
 contract ProjectManager is ExtendsOwnable, ValidValue {
 
     struct project {
@@ -25,7 +28,12 @@ contract ProjectManager is ExtendsOwnable, ValidValue {
         pictionNetwork = IPictionNetwork(pictionNetworkAddress);
     }
 
-    function deploy(string hash, string uri) external validString(hash) validString(uri) {
+    /**
+      * @dev 프로젝트 생성
+      * @param hash unique hash
+      * @param uri unique uri
+      */
+    function create(string hash, string uri) external validString(hash) validString(uri) {
         require(IValidation(pictionNetwork.getAddress(ACCOUNTMANAGER)).accountValidation(msg.sender), "ProjectManager deploy 0");
         require(!projects[hash].isRegistered, "ProjectManager deploy 1");
         require(!isDuplicateString[uri], "ProjectManager deploy 2");
@@ -39,6 +47,12 @@ contract ProjectManager is ExtendsOwnable, ValidValue {
         emit Deploy(msg.sender, hash, uri);
     }
 
+    /**
+      * @dev 기존에 생성 된 프로젝트 migration - Piction 계정 실행
+      * @param user creator public address
+      * @param hash unique hash
+      * @param uri unique uri
+      */
     function migration(address user, string hash, string uri) external onlyOwner validString(hash) validString(uri) {
         require(IValidation(pictionNetwork.getAddress(ACCOUNTMANAGER)).accountValidation(user), "ProjectManager migration 0");
         require(!projects[hash].isRegistered, "ProjectManager migration 1");
@@ -53,14 +67,29 @@ contract ProjectManager is ExtendsOwnable, ValidValue {
         emit Migration(msg.sender, user, hash, uri);
     }
 
+    /**
+      * @dev 프로젝트 고유 값 확인
+      * @param str hash or uri
+      * @return 고유 정보 중복 여부
+      */
     function stringValidation(string str) external view returns(bool) {
         return isDuplicateString[str];
     }
 
+    /**
+      * @dev creator public address 확인
+      * @param hash unique hash
+      * @return creator public address
+      */
     function getProjectOwner(string hash) external view returns(address) {
         return projects[hash].wallet;
     }
 
+    /**
+      * @dev 프로젝트 상세 정보 확인
+      * @param hash unique hash
+      * @return 프로젝트 상세 정보
+      */
     function getProject(string hash) external view returns(bool, address, string) {
         return (projects[hash].isRegistered, projects[hash].wallet, projects[hash].uri);
     }
